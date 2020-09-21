9c13628cb4eddc5d0f07e923d83f877de45d206b
adimarci <adimarci1@gmail.com>
2020-09-21 14:25:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace VerzioKezelesHF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("adja meg a hosszat");
+            int bekertSzamok = int.Parse(Console.ReadLine());
+            int[] szamok = new int[bekertSzamok];
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                Console.WriteLine("Adjon meg a"+i+". számot");
+                szamok[i] =int.Parse(Console.ReadLine());
+            }
+            int sum = 0;
+            int avg = 0;
+            int max = 0;
+           // max = Math.Max(szamok[]);
+            foreach (int f  in szamok)
+            {
+                
+                sum += f;
+                
+            }
+            avg = sum / szamok.Length;
+            Console.WriteLine(avg);
+            
+
+            Console.ReadKey();
+        
+        }
+    }
+}
